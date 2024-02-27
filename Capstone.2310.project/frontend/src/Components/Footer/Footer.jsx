d8fde517a60c406964f78802e8ceba94a6a53adf
Hinson7886 <JonHinsonfx@gmail.com>
2024-02-26 21:06:16 -0500

    test in the footer

--- Capstone.2310.project/frontend/src/Components/Footer/Footer.jsx
+++ Capstone.2310.project/frontend/src/Components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import './footer.css'
 function Footer() {
 
     return (
-        <h1>hello from footer</h1>
+        <>
+            <h1>hello from footer</h1>
+            <h1>test</h1>
+        </>
+
     )
 }
 
