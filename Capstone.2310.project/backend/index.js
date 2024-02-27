a4870f3f188b43389d117d16752e24f722426bec
lilytava2510 <tavarezlily10@gmail.com>
2024-02-27 01:46:05 -0500

    just backend base code

---
+++ Capstone.2310.project/backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use('/api', require('./routes'));
+
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
