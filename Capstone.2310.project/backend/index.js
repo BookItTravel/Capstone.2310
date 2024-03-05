aa463d0008f40ad20e6c89e55ca06a08de0c4329
lilytava2510 <tavarezlily10@gmail.com>
2024-03-04 20:51:14 -0500

    created api calls for flight, destination, airports, hotels

--- Capstone.2310.project/backend/index.js
+++ Capstone.2310.project/backend/index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-
-const PORT = process.env.PORT || 3000;
-
+const express = require("express");
+const router = require("./api/router");
+const PORT = 3000;
 const app = express();
 
+// Apply JSON parsing middleware
+app.use(express.json());
+// Apply router
+app.use("/", router);
 
-app.use('/api', require('./routes'));
 
-
-app.use(cors());
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Serving app on defined PORT
+app.listen(PORT, () => {
+  console.log(`Express is running on port ${PORT}`);
+});
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
