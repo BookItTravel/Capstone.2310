c780e82d50463d274bd8b1d2038895789a5b0d2a
mj-brown <brown8mj5@gmail.com>
2024-03-09 14:10:29 -1000

    post merge, fontawesome installed, login/register not commented out.

--- Capstone.2310.project/frontend/src/Components/Navbar/Navbar.jsx
+++ Capstone.2310.project/frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import './navbar.scss'
 import { MdOutlineTravelExplore } from 'react-icons/md';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { TbGridDots } from 'react-icons/tb';
+
 function Navbar() {
 
     return (
