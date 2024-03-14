903959b94c1aa0c1c83b48767eba8aac87fdb8a0
Hinson7886 <JonHinsonfx@gmail.com>
2024-03-13 21:22:01 -0400

    added login /register to dev02 branch

--- Capstone.2310.project/frontend/src/App.jsx
+++ Capstone.2310.project/frontend/src/App.jsx
@@ -5,23 +5,33 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Main from './Components/Main/Main'
 import Footer from './Components/Footer/Footer'
-// import Register from './Components/Register/Register'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
 import Flight_Main from './Components/Flight/Flight_Main'
 import Profile from './Pages/Profile/Profile'
 // import Flight_Search from './Components/Flight_Search/Flight_Search'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
       <Navbar />
       <Home />
       <Main />
-      <Footer />
-      <Profile />
-      <Flight_Main />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Profile />
+        <Flight_Main />
+      </BrowserRouter>
     </>
   )
 }
