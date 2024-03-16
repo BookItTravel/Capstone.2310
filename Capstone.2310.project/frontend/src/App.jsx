accb64248ef5a3b5e9c7c25cd7b33da24c2a2801
mj-brown <brown8mj5@gmail.com>
2024-03-16 13:25:58 -1000

    framework for booking flow created and course styling done.  Also pushing up to try and fix issue with registering.

--- Capstone.2310.project/frontend/src/App.jsx
+++ Capstone.2310.project/frontend/src/App.jsx
@@ -8,6 +8,8 @@ import Footer from './Components/Footer/Footer'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
 import Booking from './Pages/Booking/Booking'
+import BookingDetails from './Pages/BookingDetails/BookingDetails'
+import Confirmation from './Pages/Confirmation/Confirmation'
 import Flight_Main from './Components/Flight/Flight_Main'
 import Profile from './Pages/Profile/Profile'
 // import Flight_Search from './Components/Flight_Search/Flight_Search'
@@ -34,8 +36,10 @@ function App() {
           {/* <Route path="/" element={<Home />} />
           <Route path="/main" element={<Main />} /> */}
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />3
-          <Route path="/booking" element={<Booking />} />3
+          <Route path="/login" element={<Login />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/bookingdetails" element={<BookingDetails />} />
+          <Route path="/confirmation" element={<Confirmation />} />
           {/* <Route path="/" element={<Main />} /> */}
         </Routes>
         <Profile />
