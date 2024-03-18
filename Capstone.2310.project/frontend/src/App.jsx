d14547352e3d72cb1659d8b5c1274144f444b62d
lilytava2510 <tavarezlily10@gmail.com>
2024-03-18 19:30:43 -0400

    searching for flight is working

--- Capstone.2310.project/frontend/src/App.jsx
+++ Capstone.2310.project/frontend/src/App.jsx
@@ -10,11 +10,12 @@ import Register from './Components/Register/Register'
 import Booking from './Pages/Booking/Booking'
 import BookingDetails from './Pages/BookingDetails/BookingDetails'
 import Confirmation from './Pages/Confirmation/Confirmation'
-import Flight_Main from './Components/Flight/Flight_Main'
+//import Flight_Main from './Components/Flight/Flight_Main'
 import Profile from './Pages/Profile/Profile'
-// import Flight_Search from './Components/Flight_Search/Flight_Search'
+ import Flight_Search from './Components/Flight_Search/Flight_Search'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Flights from './Components/Flight/flight'
 
 
 function App() {
@@ -43,8 +44,10 @@ function App() {
           {/* <Route path="/" element={<Main />} /> */}
         </Routes>
         <Profile />
-        <Flight_Main />
+        {/* <Flight_Main /> */}
+      <Flight_Search />
       </BrowserRouter>
+      <Flights />
       <Footer />
     </>
   )
