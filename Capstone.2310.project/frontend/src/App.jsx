f0eccd3262d9ce22df05075f37ad7d650df2163b
mj-brown <brown8mj5@gmail.com>
2024-04-01 19:04:01 -1000

    started to add stripe to the project

--- Capstone.2310.project/frontend/src/App.jsx
+++ Capstone.2310.project/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import LandingPage from './Components/LandingPage/LandingPage'
@@ -9,7 +9,7 @@ import BookingDetails from './Pages/BookingDetails/BookingDetails'
 import Confirmation from './Pages/Confirmation/Confirmation'
 import Profile from './Pages/Profile/Profile'
 import Master_Table from './Components/Master_Table/Master_Table'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Flights from './Components/Flight_Search/Flights'
 import Airport from './Components/Flight/airport'
 import {loadStripe} from '@stripe/stripe-js';
@@ -20,11 +20,69 @@ import {
 
 const stripePromise = loadStripe("pk_test_51P0ushRsNEKpqF1rwE3Z5A4ExGMbur7Ca9cGRRsfvMu0U1QPxIc0yttLGCiDgUOuqKxA1yB8wh5TlpJybmaLi9cL00rMQc5pE1")
 
+const CheckoutForm = () => {
+  const fetchClientSecret = useCallback(() => {
+    // Create a Checkout Session
+    return fetch("/create-checkout-session", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, []);
+
+  const options = {fetchClientSecret};
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={options}
+      >
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  )
+}
+
+const Return = () => {
+  const [status, setStatus] = useState(null);
+  const [customerEmail, setCustomerEmail] = useState('');
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const sessionId = urlParams.get('session_id');
+
+    fetch(`/session-status?session_id=${sessionId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setStatus(data.status);
+        setCustomerEmail(data.customer_email);
+      });
+  }, []);
+
+  if (status === 'open') {
+    return (
+      <Navigate to="/checkout" />
+    )
+  }
+
+  if (status === 'complete') {
+    return (
+      <section id="success">
+        <p>
+          We appreciate your business! A confirmation email will be sent to {customerEmail}.
+
+          If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
+        </p>
+      </section>
+    )
+  }
+
+  return null;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
-
-
-
 
   return (
     <>
