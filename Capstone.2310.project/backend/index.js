0d316ff57fef0b42fd7622205699b5ba0af9a973
lilytava2510 <tavarezlily10@gmail.com>
2024-04-15 13:50:06 -0400

    added deploy url

--- Capstone.2310.project/backend/index.js
+++ Capstone.2310.project/backend/index.js
@@ -14,7 +14,7 @@ const router = require('./api/router');
 const {
   PORT = 3000,
   JWT_SECRET = 'placeholder secret',
-  DEPLOYED_URL = 'http://localhost:5173'
+  DEPLOYED_URL = 'https://travelapp-3da1f049e629.herokuapp.com/'
 } = process.env;
 
 // Apply JSON parsing middleware
